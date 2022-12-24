2d3b37237b5007a68f1f730ab04388506760f15f
Forketyfork <forketyfork@icloud.com>
2022-12-24 09:54:48 +0100

    Day 24 fixed a bug

--- src/main/kotlin/year2022/Day24.kt
+++ src/main/kotlin/year2022/Day24.kt
@@ -13,8 +13,8 @@ class Day24(contents: String) {
     private val lines = contents.lines()
     private val width = lines.first().length
     private val height = lines.size
-    private val source = Point2D(1, 0)
-    private val destination = Point2D(width - 2, height - 1)
+    private val entrance = Point2D(1, 0)
+    private val exit = Point2D(width - 2, height - 1)
     private val blizzardPeriod = (width - 2).lcm(height - 2)
     private val blizzardsByStep = mapOf(
         0 to lines.flatMapIndexed { row, line ->
@@ -24,17 +24,17 @@ class Day24(contents: String) {
         }.toMap()
     ).toMutableMap()
 
-    fun part1() = solve(State(0, source), destination)
+    fun part1() = solve(State(0, entrance), exit)
 
-    fun part2() = listOf(source, destination, source, destination)
+    fun part2() = listOf(entrance, exit, entrance, exit)
         .windowed(2)
         .fold(0) { step, (from, to) -> solve(State(step, from), to) }
 
-    private fun solve(initialState: State, dest: Point2D): Int = with(ArrayDeque(setOf(initialState))) {
+    private fun solve(initialState: State, destination: Point2D): Int = with(ArrayDeque(setOf(initialState))) {
         val seenStates = mutableMapOf<Point2D, MutableSet<Int>>()
         while (!isEmpty()) {
             val state = removeFirst()
-            if (state.position == dest) {
+            if (state.position == destination) {
                 return state.step - 1
             }
             if (seenStates.getOrPut(state.position) { mutableSetOf() }.add(state.step % blizzardPeriod)) {
@@ -46,9 +46,9 @@ class Day24(contents: String) {
                                     it.copy(x = 1)
                                 } else if (it.x == 0) {
                                     it.copy(x = width - 2)
-                                } else if (it != dest && it.y == height - 1 || it == dest) {
+                                } else if (it != exit && it.y == height - 1 || it == exit) {
                                     it.copy(y = 1)
-                                } else if (it != source && it.y == 0 || it == source) {
+                                } else if (it != entrance && it.y == 0 || it == entrance) {
                                     it.copy(y = height - 2)
                                 } else {
                                     it
@@ -61,7 +61,7 @@ class Day24(contents: String) {
                 sequenceOf(DOWN, LEFT, UP, RIGHT)
                     .map { dir -> state.position.move(dir) }
                     .filter {
-                        it == dest || it.x in (1..width - 2) && it.y in (1..height - 2)
+                        it == entrance || it == exit || it.x in (1..width - 2) && it.y in (1..height - 2)
                     }
                     .plus(state.position)
                     .filter { !nextBlizzards.keys.contains(it) }
