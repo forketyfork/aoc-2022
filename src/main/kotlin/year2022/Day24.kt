631f6c774100cdf54eb42a25912c5637f789c112
Forketyfork <forketyfork@icloud.com>
2022-12-24 09:49:43 +0100

    Day 24 optimization

--- src/main/kotlin/year2022/Day24.kt
+++ src/main/kotlin/year2022/Day24.kt
@@ -19,7 +19,7 @@ class Day24(contents: String) {
     private val blizzardsByStep = mapOf(
         0 to lines.flatMapIndexed { row, line ->
             line.withIndex().filter { it.value in Direction.ARROWS }.map { (col, char) ->
-                Point2D(col, row) to setOf(Blizzard(Point2D(col, row), Direction.byArrow(char)))
+                Point2D(col, row) to listOf(Blizzard(Point2D(col, row), Direction.byArrow(char)))
             }
         }.toMap()
     ).toMutableMap()
@@ -30,49 +30,43 @@ class Day24(contents: String) {
         .windowed(2)
         .fold(0) { step, (from, to) -> solve(State(step, from), to) }
 
-
-    private fun solve(initialState: State, dest: Point2D): Int = with(ArrayDeque<State>()) {
+    private fun solve(initialState: State, dest: Point2D): Int = with(ArrayDeque(setOf(initialState))) {
         val seenStates = mutableMapOf<Point2D, MutableSet<Int>>()
-        add(initialState)
         while (!isEmpty()) {
             val state = removeFirst()
             if (state.position == dest) {
                 return state.step - 1
             }
-            if (!seenStates.getOrPut(state.position) { mutableSetOf() }.add(state.step % blizzardPeriod)) {
-                continue
-            }
-
-            val nextBlizzards = blizzardsByStep.computeIfAbsent(state.step % blizzardPeriod) {
-                blizzardsByStep[state.step - 1]!!.values.flatMap { blizzards ->
-                    blizzards.map { blizzard ->
-                        val nextPosition = blizzard.position.move(blizzard.direction).let {
-                            if (it.x == width - 1) {
-                                it.copy(x = 1)
-                            } else if (it.x == 0) {
-                                it.copy(x = width - 2)
-                            } else if (it != dest && it.y == height - 1 || it == dest) {
-                                it.copy(y = 1)
-                            } else if (it != source && it.y == 0 || it == source) {
-                                it.copy(y = height - 2)
-                            } else {
-                                it
+            if (seenStates.getOrPut(state.position) { mutableSetOf() }.add(state.step % blizzardPeriod)) {
+                val nextBlizzards = blizzardsByStep.computeIfAbsent(state.step % blizzardPeriod) {
+                    blizzardsByStep[state.step - 1]!!.values.flatMap { blizzards ->
+                        blizzards.map { blizzard ->
+                            val nextPosition = blizzard.position.move(blizzard.direction).let {
+                                if (it.x == width - 1) {
+                                    it.copy(x = 1)
+                                } else if (it.x == 0) {
+                                    it.copy(x = width - 2)
+                                } else if (it != dest && it.y == height - 1 || it == dest) {
+                                    it.copy(y = 1)
+                                } else if (it != source && it.y == 0 || it == source) {
+                                    it.copy(y = height - 2)
+                                } else {
+                                    it
+                                }
                             }
+                            nextPosition to blizzard.copy(position = nextPosition)
                         }
-                        nextPosition to blizzard.copy(position = nextPosition)
-                    }
-                }.groupBy({ it.first }, { it.second })
-                    .map { it.key to it.value.toSet() }
-                    .toMap()
-            }
-            sequenceOf(DOWN, LEFT, UP, RIGHT)
-                .map { dir -> state.position.move(dir) }
-                .filter {
-                    it == dest || it.x in (1..width - 2) && it.y in (1..height - 2)
+                    }.groupBy({ it.first }, { it.second }).toMap()
                 }
-                .plus(state.position)
-                .filter { !nextBlizzards.keys.contains(it) }
-                .forEach { nextPosition -> addLast(State(state.step + 1, nextPosition)) }
+                sequenceOf(DOWN, LEFT, UP, RIGHT)
+                    .map { dir -> state.position.move(dir) }
+                    .filter {
+                        it == dest || it.x in (1..width - 2) && it.y in (1..height - 2)
+                    }
+                    .plus(state.position)
+                    .filter { !nextBlizzards.keys.contains(it) }
+                    .forEach { nextPosition -> addLast(State(state.step + 1, nextPosition)) }
+            }
         }
         error("Solution not found")
     }
