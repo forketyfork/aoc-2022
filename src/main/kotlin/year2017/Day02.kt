9ead6d98f18ddde085f07634cdf51788b6551ab9
Forketyfork <forketyfork@icloud.com>
2022-12-27 09:55:09 +0100

    2017.02.2

--- src/main/kotlin/year2017/Day02.kt
+++ src/main/kotlin/year2017/Day02.kt
@@ -2,10 +2,17 @@ package year2017
 
 class Day02 {
 
-    fun part1(input: String) = input.lines()
-        .map { it.split(" ", "\t").map(String::toInt) }
-        .sumOf { it.max() - it.min() }
+    fun part1(input: String) = parse(input).sumOf { it.max() - it.min() }
 
-    fun part2(input: String) = 0 // TODO
+    fun part2(input: String) = parse(input).map { it.sorted() }.sumOf {
+        (0..<it.lastIndex).flatMap { i1 ->
+            (i1 + 1..it.lastIndex).map { i2 -> it[i1] to it[i2] }
+        }.first { (num1, num2) -> num2 % num1 == 0 }
+            .let { (num1, num2) -> num2 / num1 }
+
+    }
+
+    private fun parse(input: String) = input.lines()
+        .map { it.split(" ", "\t").map(String::toInt) }
 
 }
--- src/test/kotlin/year2017/Day02Test.kt
+++ src/test/kotlin/year2017/Day02Test.kt
@@ -14,17 +14,20 @@ class Day02Test {
         @JvmStatic
         fun data() = cases(
             arrayOf(
-                Arguments.of("year2017/Day02_sample", 18, 0),
+                Arguments.of("year2017/Day02_sample1", 18, null),
+                Arguments.of("year2017/Day02_sample2", 18, 9),
             ),
-            Arguments.of("year2017/Day02_personal", 39126, 0)
+            Arguments.of("year2017/Day02_personal", 39126, 258)
         )
     }
 
     @ParameterizedTest(name = "File {0}, part1Answer = {1}, part2Answer = {2}")
     @MethodSource("data")
-    fun test(file: String, part1Answer: Int, part2Answer: Int) {
+    fun test(file: String, part1Answer: Int, part2Answer: Int?) {
         val contents = readTestFile(file)
         assertThat(Day02().part1(contents)).isEqualTo(part1Answer)
-        assertThat(Day02().part2(contents)).isEqualTo(part2Answer)
+        if (part2Answer != null) {
+            assertThat(Day02().part2(contents)).isEqualTo(part2Answer)
+        }
     }
 }
