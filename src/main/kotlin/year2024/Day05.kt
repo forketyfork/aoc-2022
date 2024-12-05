1b91485d4c7ff1909a50d8192418094f710faccd
Forketyfork <forketyfork@icloud.com>
2024-12-05 12:55:18 +0100

    2024-05

--- src/main/kotlin/year2024/Day05.kt
+++ src/main/kotlin/year2024/Day05.kt
@@ -7,25 +7,12 @@ class Day05 {
     fun part2(input: String) = solve(input, false)
 
     fun solve(input: String, correct: Boolean): Long {
-        return partition(input)
+        val (ordering, updates) = input.split("\n\n")
+        val order = ordering.lines().toSet()
+        return updates.lines()
+            .map { it.split(",") }
+            .map { it to it.sortedWith { o1, o2 -> if ("$o1|$o2" in order) -1 else if ("$o2|$o1" in order) 1 else 0 } }
             .filter { (it.first == it.second) == correct }
             .sumOf { it.second[it.second.size / 2].toLong() }
     }
-
-    fun partition(input: String): List<Pair<List<String>, List<String>>> {
-        val (ordering, updates) = input.split("\n\n")
-        val orderingSet = ordering.lines().toSet()
-        return updates.lines().map { it.split(",") }.map {
-            it to it.sortedWith { o1, o2 ->
-                if (orderingSet.contains("$o1|$o2")) {
-                    -1
-                } else if (orderingSet.contains("$o2|$o1")) {
-                    1
-                } else {
-                    0
-                }
-            }
-        }
-    }
-
 }
