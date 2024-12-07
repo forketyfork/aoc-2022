e43904abd469433f849c684577a11775dc9ad94c
Forketyfork <forketyfork@icloud.com>
2024-12-07 07:41:07 +0100

    2024-07 Removed nullability

--- src/main/kotlin/year2024/Day07.kt
+++ src/main/kotlin/year2024/Day07.kt
@@ -4,15 +4,15 @@ class Day07 {
 
     fun part1(input: String) = solve(input, listOf(Long::times, Long::plus))
 
-    fun part2(input: String) = solve(input, listOf(Long::times, Long::plus, { a, b -> "$a$b".toLongOrNull() }))
+    fun part2(input: String) = solve(input, listOf(Long::times, Long::plus, { a, b -> "$a$b".toLong() }))
 
-    fun solve(input: String, ops: List<(Long, Long) -> Long?>) = input.lines()
+    fun solve(input: String, ops: List<(Long, Long) -> Long>) = input.lines()
         .map { it.split(":?\\s".toRegex()).map { it.toLong() } }
         .filter { rec(it, ops) }
         .sumOf { it.first() }
 
-    fun rec(values: List<Long>, ops: List<(Long, Long) -> Long?>, idx: Int = 1, sum: Long? = values[1]): Boolean = when {
-        sum == null || sum > values[0] -> false
+    fun rec(values: List<Long>, ops: List<(Long, Long) -> Long>, idx: Int = 1, sum: Long = values[1]): Boolean = when {
+        sum > values[0] -> false
         idx == values.lastIndex -> sum == values[0]
         else -> ops.any { op -> rec(values, ops, idx + 1, op(sum, values[idx + 1])) }
     }
