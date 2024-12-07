7c805bb4ad13e02739bc54895d196a8e10fe9872
Forketyfork <forketyfork@icloud.com>
2024-12-07 07:46:02 +0100

    2024-07 Cleaned up

--- src/main/kotlin/year2024/Day07.kt
+++ src/main/kotlin/year2024/Day07.kt
@@ -2,19 +2,19 @@ package year2024
 
 class Day07 {
 
-    fun part1(input: String) = solve(input, listOf(Long::times, Long::plus))
+    fun part1(input: String) = solve(input, Long::times, Long::plus)
 
-    fun part2(input: String) = solve(input, listOf(Long::times, Long::plus, { a, b -> "$a$b".toLong() }))
+    fun part2(input: String) = solve(input, Long::times, Long::plus, { a, b -> "$a$b".toLong() })
 
-    fun solve(input: String, ops: List<(Long, Long) -> Long>) = input.lines()
+    fun solve(input: String, vararg ops: (Long, Long) -> Long) = input.lines()
         .map { it.split(":?\\s".toRegex()).map { it.toLong() } }
-        .filter { rec(it, ops) }
+        .filter { rec(it, 1, it[1], *ops) }
         .sumOf { it.first() }
 
-    fun rec(values: List<Long>, ops: List<(Long, Long) -> Long>, idx: Int = 1, sum: Long = values[1]): Boolean = when {
+    fun rec(values: List<Long>, idx: Int, sum: Long, vararg ops: (Long, Long) -> Long): Boolean = when {
         sum > values[0] -> false
         idx == values.lastIndex -> sum == values[0]
-        else -> ops.any { rec(values, ops, idx + 1, it(sum, values[idx + 1])) }
+        else -> ops.any { rec(values, idx + 1, it(sum, values[idx + 1]), *ops) }
     }
 
 }
