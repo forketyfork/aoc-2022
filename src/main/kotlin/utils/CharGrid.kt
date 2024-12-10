ca34161411335ce0ff35aaaeaad84bd671c1e0e5
Forketyfork <forketyfork@icloud.com>
2024-12-10 07:24:18 +0100

    Better CharGrid points() function, now returns a Sequence to avoid keeping all points in memory.

--- src/main/kotlin/utils/CharGrid.kt
+++ src/main/kotlin/utils/CharGrid.kt
@@ -4,9 +4,15 @@ class CharGrid(val input: String) {
 
     val grid = input.lines().map { it.toCharArray() }
 
-    fun points(): List<Point2D> = grid.indices.flatMap { y ->
-        grid[y].indices.map { x ->
-            Point2D(x, y)
+    fun points(): Sequence<Point2D> = generateSequence(Point2D(0, 0)) {
+        if (it.x == grid[it.y].lastIndex) {
+            if (it.y == grid.lastIndex) {
+                null
+            } else {
+                Point2D(0, it.y + 1)
+            }
+        } else {
+            Point2D(it.x + 1, it.y)
         }
     }
 
