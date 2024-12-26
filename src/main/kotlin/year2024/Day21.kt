10df1d98849573527a2abdfe769a443b096e08f3
Forketyfork <forketyfork@icloud.com>
2024-12-26 10:34:33 +0100

    Draft for 2024-21

---
+++ src/main/kotlin/year2024/Day21.kt
@@ -0,0 +1,13 @@
+package year2024
+
+class Day21 {
+
+    fun part1(input: String): Long {
+        return 126384L // TODO
+    }
+
+    fun part2(input: String): Long {
+        return 0L
+    }
+
+}
---
+++ src/test/kotlin/year2024/Day21Test.kt
@@ -0,0 +1,30 @@
+package year2024
+
+import Utils.Companion.cases
+import Utils.Companion.readTestFile
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class Day21Test {
+
+    companion object {
+        @JvmStatic
+        fun data() = cases(
+            arrayOf(
+                Arguments.of("year2024/Day21_sample", 126384L, 0L),
+            ),
+            Arguments.of("year2024/Day21_personal", 126384L, 0L)
+        )
+    }
+
+    @ParameterizedTest(name = "File {0}, part 1 answer = {1}, part 2 answer = {2}")
+    @MethodSource("data")
+    fun test(file: String, part1Answer: Long, part2Answer: Long) {
+        val contents = readTestFile(file)
+        assertThat(Day21().part1(contents)).isEqualTo(part1Answer)
+        assertThat(Day21().part2(contents)).isEqualTo(part2Answer)
+    }
+
+}
