2c8ead897495d259b06c3b34f7d17889f6aea005
Forketyfork <forketyfork@icloud.com>
2024-12-29 14:09:12 +0100

    2024-20, part 1 optimization

--- src/main/kotlin/year2024/Day20.kt
+++ src/main/kotlin/year2024/Day20.kt
@@ -2,15 +2,39 @@ package year2024
 
 import utils.CharGrid
 import utils.Direction
+import utils.Point2D
 
 class Day20 {
+
+    fun CharGrid.calculateDistances(start: Point2D) = buildMap {
+        val queue = ArrayDeque<Pair<Point2D, Int>>()
+        queue.add(start to 0)
+        put(start, 0)
+        while (queue.isNotEmpty()) {
+            val (next, nextDistance) = queue.removeFirst()
+            if (this@calculateDistances[next] != '#') {
+                val startToNeighbour = nextDistance + 1
+                Direction.SQUARE.forEach { dir ->
+                    val neighbour = next.move(dir)
+                    if (inBounds(neighbour) && !containsKey(neighbour)) {
+                        queue.add(neighbour to startToNeighbour)
+                        put(neighbour, startToNeighbour)
+                    }
+                }
+            }
+        }
+    }
 
     fun part1(input: String, minSavedTime: Int): Int {
 
         val grid = CharGrid(input)
         val start = grid.findFirst('S')
         val end = grid.findFirst('E')
-        val shortest = grid.bfs(start, stopWhen = { it == end }, canMove = { _, to -> grid[to] != '#' }).size
+
+        val startCache = grid.calculateDistances(start)
+        val endCache = grid.calculateDistances(end)
+
+        val shortest = startCache[end]!!
 
         return grid.pointSequence().flatMap { cheatStart ->
             Direction.SQUARE.map { dir ->
@@ -19,20 +43,14 @@ class Day20 {
         }
             .filter { (cheatStart, cheatEnd) -> grid.inBorder(cheatStart) && grid.inBorder(cheatEnd) && grid[cheatStart] == '#' && grid[cheatEnd] != '#' }
             .map { (cheatStart, cheatEnd) ->
-                grid.resetMarks()
-                grid[cheatStart] = '.'
-                val path = grid.bfs(start, stopWhen = { it == end }, canMove = { _, to -> grid[to] != '#' })
-                grid[cheatStart] = '#'
-                val cheatStartIdx = path.indexOf(cheatStart)
-                if (cheatStartIdx != -1 && cheatStartIdx < path.lastIndex && path[cheatStartIdx + 1] == cheatEnd) {
-                    shortest - path.size
-                } else {
-                    0
-                }
-            }.filter { it != 0 }
+                val path1Len = startCache[cheatStart]!!
+                val path2Len = endCache[cheatEnd]!!
+                shortest - (path1Len + 1 + path2Len)
+            }.filter { it > 0 }
             .groupBy { it }
             .mapValues { (_, value) -> value.size }
-            .filterKeys { it >= minSavedTime }.values.sum()
+            .filterKeys { it >= minSavedTime }
+            .values.sum()
     }
 
     fun part2(input: String): Long {
